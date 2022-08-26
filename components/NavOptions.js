db3e9964f8328f210b11757fb00a0ad62db6d899
Vitor Hugo Patzlaff Macedo <vitorpatzlaff@gmail.com>
2022-08-25 23:43:37 -0300

    implement google places autocomplete api

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import {
   FlatList,
+  Image,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  View
 } from 'react-native'
+import { Icon } from 'react-native-elements'
 import tw from 'twrnc'
 
 const data = [
@@ -16,19 +20,37 @@ const data = [
   {
     id: '456',
     title: 'Order food',
-    image: 'https://linkds.papareact.com/28w',
+    image: 'https://cdn3.iconfinder.com/data/icons/takeaway-food/500/nim1413_24_noodle_box_chinese_food-512.png',
     screen: 'EatScreen'
   }
 ]
 
 const NavOptions = () => {
+  const navigation = useNavigation()
+
   return (
     <FlatList
       data={data}
+      horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.title}</Text>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          onPress={() => navigation.navigate(item.screen)}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright'
+              color='white'
+              type='antdesign'
+            />
+          </View>
         </TouchableOpacity>
       )}
     />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,9 +5,15 @@ import {
   View,
 } from 'react-native'
 import tw from 'twrnc'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -20,6 +26,38 @@ const HomeScreen = () => {
           source={{
             uri: 'https://links.papareact.com/gzs'
           }}
+          />
+
+        <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description
+              })
+            )
+
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType='search'
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
         />
 
         <NavOptions />
